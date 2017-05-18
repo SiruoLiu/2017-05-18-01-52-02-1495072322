dcff561a8a9fe0b855a9e86fdf4b045afd4de955
SiruoLiu <1143398076@qq.com>
2017-05-18 21:26:41 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,16 @@
 // Write your code here
 module.exports = Person
+/*
+ * 定义一个类User,js中使用构造函数实现 
+ */  
+Person = function(sname,sage){  
+    //定义属性  
+    this.name = sname;  
+    this.age = sage;  
+ }  
+  
+// js通过prototype动态为类添加方法  
+User.prototype.introduce = function(){  
+  var a="My name is "+name+". I am "+age+" years old."
+    return a;  
+};  
