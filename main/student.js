30fd2771dd4e9ce1653f466d49b7977879590220
SiruoLiu <1143398076@qq.com>
2017-05-19 15:12:27 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,3 +1,17 @@
-let Person = require('./person')
+ let Person = require('./person')
 // Write your code here
 module.exports = Student
+ //define a sub class
+var Student = function(name,age,kclass){
+    this.kclass = kclass;
+    Person.prototype.constructor.call(this,name,age);
+};
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+Student.baseClass = Person;
+//override the sayHello method
+     Student.prototype.introduce  = function(){
+    //call the method in the base class
+     var a="My name is "+this.name+". I am "+this.age+" years old."+" I am at Class "+this.kclass+"."  
+    return a;  
+};
