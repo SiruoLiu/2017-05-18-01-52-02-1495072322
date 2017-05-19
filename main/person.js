14f94262fde8af07458c4645818195b2e800a5bb
SiruoLiu <1143398076@qq.com>
2017-05-19 15:54:17 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -7,10 +7,16 @@ var Person = function(name,age){
     //定义属性  
     this.name = name;  
     this.age = age;  
- }  
+ };  
   
 // js通过prototype动态为类添加方法  
 Person.prototype.introduce = function(){  
   var a="My name is "+this.name+". I am "+this.age+" years old."
     return a;  
 };  
+ var Person = function(name,age){
+      //instance property, which owns by the instance.
+      this.name = name;
+      this.age = age;
+  };
+ 
