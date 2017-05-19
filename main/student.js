45ff600c2f2fd15222eab0168bf7c45a00cd761b
SiruoLiu <1143398076@qq.com>
2017-05-19 20:21:40 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -3,8 +3,9 @@
 module.exports = Student
  //define a sub class
 var Student = function(name,age,kclass){
-    this.kclass = kclass;
+    
     Person.prototype.constructor.call(this,name,age);
+    this.kclass = kclass;
 };
 Student.prototype = Object.create(Person.prototype);
 //Student.prototype = new Person(name,age) ;
@@ -13,6 +14,7 @@ Student.baseClass = Person;
 //override the sayHello method
      Student.prototype.introduce  = function(){
     //call the method in the base class
+         
      var a="My name is "+this.name+". I am "+this.age+" years old."+" I am at Class "+this.kclass+"."  
     return a;  
 };
